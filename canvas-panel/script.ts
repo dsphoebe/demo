f889deebcff6d2c0518dd5c53c057ed49fd98eee
dsphoebe <i@dsphoebe.com>
2018-12-04 18:37:20 +0800

    支持手机

--- canvas-panel/script.ts
+++ canvas-panel/script.ts
@@ -1,7 +1,9 @@
 const canvas = <HTMLCanvasElement> document.querySelector('#canvas')
 const context = canvas.getContext('2d')
+const isSupportTouch = 'ontouchstart' in document.documentElement
 
 autoSetCanvasSize(canvas)
+listenToUser()
 
 function autoSetCanvasSize(canvas) {
   setCanvasSize()
@@ -19,106 +21,144 @@ function autoSetCanvasSize(canvas) {
   }
 }
 
-let enableeraser = false
-let enablePen = false
-let penSize = 5
-let eraserSize = penSize * 15
-let color = 'black'
-let lastPoint = {
-  x: undefined,
-  y: undefined
-}
+function listenToUser() {
+  let enableeraser = false
+  let enablePen = false
+  let penSize = 5
+  let eraserSize = penSize * 15
+  let color = 'black'
+  let lastPoint = {
+    x: undefined,
+    y: undefined
+  }
 
-const docCSS = getComputedStyle(document.documentElement)
-const hdHeight = parseInt(docCSS.getPropertyValue('--hd-height'), 10)
-const minusHeaderHeight = (y:number) => y - hdHeight
+  const docCSS = getComputedStyle(document.documentElement)
+  const hdHeight = parseInt(docCSS.getPropertyValue('--hd-height'), 10)
+  const minusHeaderHeight = (y:number) => y - hdHeight
 
-function drawLine(x1:number, y1:number, x2:number, y2:number) {
-  context.beginPath()
-  context.strokeStyle = color
-  context.moveTo(x1, minusHeaderHeight(y1))
-  context.lineWidth = penSize
-  context.lineTo(x2, minusHeaderHeight(y2))
-  context.stroke()
-  context.closePath()
-}
+  function drawLine(x1:number, y1:number, x2:number, y2:number) {
+    context.beginPath()
+    context.strokeStyle = color
+    context.moveTo(x1, minusHeaderHeight(y1))
+    context.lineWidth = penSize
+    context.lineTo(x2, minusHeaderHeight(y2))
+    context.stroke()
+    context.closePath()
+  }
 
-canvas.onmousedown = function(event) {
-  const x = event.clientX
-  const y = event.clientY
+  canvas.ontouchstart = function(event) {
+    const x = event.touches[0].clientX
+    const y = event.touches[0].clientY
 
-  enablePen = true
+    enablePen = true
 
-  if (enableeraser) {
-    context.clearRect(x - (eraserSize/2), minusHeaderHeight(y - (eraserSize/2)), eraserSize, eraserSize)
-  } else {
-    lastPoint = {
-      x, y
+    if (enableeraser) {
+      context.clearRect(x - (eraserSize/2), minusHeaderHeight(y - (eraserSize/2)), eraserSize, eraserSize)
+    } else {
+      lastPoint = {
+        x, y
+      }
     }
   }
-}
 
-canvas.onmousemove = function(event) {
-  if (!enablePen) {
-    return
-  }
+  canvas.onmousedown = function(event) {
+    const x = event.clientX
+    const y = event.clientY
 
-  const x = event.clientX
-  const y = event.clientY
+    enablePen = true
 
-  if (enableeraser) {
-    context.clearRect(x - (eraserSize/2), minusHeaderHeight(y - (eraserSize/2)), eraserSize, eraserSize)
-  } else {
-    const newPoint = {
-      x, y
+    if (enableeraser) {
+      context.clearRect(x - (eraserSize/2), minusHeaderHeight(y - (eraserSize/2)), eraserSize, eraserSize)
+    } else {
+      lastPoint = {
+        x, y
+      }
     }
-    drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
-    lastPoint = newPoint
   }
-}
 
-canvas.onmouseup = function() {
-  enablePen = false
-}
+  canvas.onmousemove = function(event) {
+    if (!enablePen) {
+      return
+    }
 
-const colorWrap = document.querySelector('.color-wrap')
-const colors = colorWrap.querySelectorAll('span')
-const pen = document.querySelector('.pen') as HTMLElement
-const eraser = document.querySelector('.eraser') as HTMLElement
+    const x = event.clientX
+    const y = event.clientY
 
-const toolsWrap = document.querySelector('.tools')
-const tools = toolsWrap.querySelectorAll('svg')
-pen.onclick = function() {
-  const activeColor = toolsWrap.querySelector('.active')
-  activeColor.classList.remove('active')
-  pen.classList.add('active')
-  enableeraser = false
-}
-eraser.onclick = function() {
-  const activeColor = toolsWrap.querySelector('.active')
-  activeColor.classList.remove('active')
-  eraser.classList.add('active')
-  enableeraser = true
-}
+    if (enableeraser) {
+      context.clearRect(x - (eraserSize/2), minusHeaderHeight(y - (eraserSize/2)), eraserSize, eraserSize)
+    } else {
+      const newPoint = {
+        x, y
+      }
+      drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+      lastPoint = newPoint
+    }
+  }
 
-Array.from(colors).forEach(element => {
-  element.onclick = function(event) {
-    const activeColor = colorWrap.querySelector('.active')
+  canvas.ontouchmove = function(event) {
+    if (!enablePen) {
+      return
+    }
+
+    const x = event.touches[0].clientX
+    const y = event.touches[0].clientY
+
+    if (enableeraser) {
+      context.clearRect(x - (eraserSize/2), minusHeaderHeight(y - (eraserSize/2)), eraserSize, eraserSize)
+    } else {
+      const newPoint = {
+        x, y
+      }
+      drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+      lastPoint = newPoint
+    }
+  }
+
+  canvas.onmouseup = function() {
+    enablePen = false
+  }
+
+  canvas.ontouchend = function() {
+    enablePen = false
+  }
+
+  const colorWrap = document.querySelector('.color-wrap')
+  const colors = colorWrap.querySelectorAll('span')
+  const pen = document.querySelector('.pen') as HTMLElement
+  const eraser = document.querySelector('.eraser') as HTMLElement
+
+  const toolsWrap = document.querySelector('.tools')
+  const tools = toolsWrap.querySelectorAll('svg')
+  pen.onclick = function() {
+    const activeColor = toolsWrap.querySelector('.active')
     activeColor.classList.remove('active')
-    const elem = event.target as HTMLElement
-    elem.classList.add('active')
-    color = getComputedStyle(elem).getPropertyValue('background-color')
-    pen.click()
+    pen.classList.add('active')
+    enableeraser = false
   }
-})
+  eraser.onclick = function() {
+    const activeColor = toolsWrap.querySelector('.active')
+    activeColor.classList.remove('active')
+    eraser.classList.add('active')
+    enableeraser = true
+  }
 
-const brushs = toolsWrap.querySelectorAll('li')
-Array.from(brushs).forEach((brush, idx) => {
-  brush.onclick = (function(idx) {
-    return function() {
-      penSize = (idx + 1) * 5
+  Array.from(colors).forEach(element => {
+    element.onclick = function(event) {
+      const activeColor = colorWrap.querySelector('.active')
+      activeColor.classList.remove('active')
+      const elem = event.target as HTMLElement
+      elem.classList.add('active')
+      color = getComputedStyle(elem).getPropertyValue('background-color')
+      pen.click()
     }
-  })(idx)
-})
+  })
 
-
+  const brushs = toolsWrap.querySelectorAll('li')
+  Array.from(brushs).forEach((brush, idx) => {
+    brush.onclick = (function(idx) {
+      return function() {
+        penSize = (idx + 1) * 5
+      }
+    })(idx)
+  })
+}
