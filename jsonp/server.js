7dea4fd11c7205b53067cb6503b410372eb233fe
dsphoebe <i@dsphoebe.com>
2019-01-14 17:24:17 +0800

    前后端交互 -> form post

---
+++ jsonp/server.js
@@ -0,0 +1,33 @@
+const http = require('http')
+const fs = require('fs')
+const url = require('url')
+const port = process.argv[2] || 8000
+
+const server = http.createServer((request, response) => {
+  const parsedUrl = url.parse(request.url, true)
+  const path = request.url
+  const query = ''
+
+
+  console.log('HTTP 路径为 \n' + path)
+
+  if (path == '/') {
+    var string = fs.readFileSync('./index.html', 'utf8')
+    var amount = fs.readFileSync('./db', 'utf8')
+    string = string.replace('$$$amout$$$', amount)
+    response.setHeader('Content-type', 'text/html; charset=utf-8')
+    response.write(string)
+    response.end()
+  }
+  if (path == '/pay') {
+    var amount = fs.readFileSync('./db', 'utf8')
+    var newAmount = amount - 1
+    fs.writeFileSync('./db', newAmount)
+    response.write('success')
+    response.end()
+  }
+})
+
+server.listen(port)
+
+console.log('监听 ' + port)
