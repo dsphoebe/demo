1d1663c9d6bd42ce111aa5176829288bf20d2fb7
dsphoebe <i@dsphoebe.com>
2019-02-02 13:32:21 +0800

    webpack with babel-loader & sass-loader

---
+++ webpack-demo/index.js
@@ -0,0 +1,4 @@
+import { a } from './a'
+import './style.sass'
+
+alert(a)
---
+++ webpack-demo/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.sass$/,
+        use: [
+          {loader: 'style-loader'},
+          {loader: 'css-loader'},
+          {loader: 'sass-loader'}
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+}
